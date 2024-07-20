4e3500b6ee4fe6a4b0a238ae9958294c65778da5
CarstenADV <carsten.hof@alpinedata.de>
2024-07-20 08:25:15 +0200

    Contact Form: Hardening of server code

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -4,29 +4,33 @@ const { SENDGRID_API_KEY } = process.env;
 
 exports.handler = async (event) => {
   // get data from body
-  const { firstname, lastname, email, message, reason, website } = JSON.parse(event.body);
+  const { firstname, lastname, email, message, reason, website, phone } = JSON.parse(event.body);
 
   // check Honeypot field
-  if (website !== "Deine Webseite")
+  if (website !== "Deine Webseite") {
     return {
       statusCode: 400,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ msg: "Ungültige Anfrage" }),
+      body: JSON.stringify({ error: 'Ungültige Anfrage.' }),
     };
+  }
 
+  // Validate that fields are not empty
+  if (!firstname || !lastname || !email || !message || !reason) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Alle Felder außer Telefonnummer sind Pflichtfelder.' }),
+    };
+  }
 
   // set API key
   sgMail.setApiKey(SENDGRID_API_KEY);
 
   // setup data for email
-  // NOTE: THIS IS NOT SECURE. YOU NEED TO SANITIZE THE INPUTS
   const data = {
-    to: "info@alpinedata.de", // Change to your recipient (your email in this case)
-    from: "info@alpinedata.de", // Change to your verified sender
+    to: "info@alpinedata.de",
+    from: "info@alpinedata.de",
     subject: `${reason} über ADV Webseite: Neue Nachricht von ${firstname} ${lastname}`,
-    html: `${firstname} ${lastname} (${email}) schreibt: <br> <br> <p>${message}</p>`,
+    html: `${firstname} ${lastname} (E-Mail: ${email} Telefon: ${phone}) schreibt: <br> <br> <p>${message}</p>`,
   };
 
   try {
