d20381f8dbf719add7c4bb84171a8a85032ca987
CarstenADV <carsten.hof@alpinedata.de>
2024-09-10 10:07:15 +0200

    CleanUp unused index indicating files

--- scripts/copy_branch.mjs
+++ scripts/copy_branch.mjs
@@ -1,7 +1,15 @@
-import { copyFile } from 'fs';
+import { copyFile, unlink } from 'fs';
 
 // File destination.txt will be created or overwritten by default.
 copyFile('./public/robot_branch.txt', 'dist/robots.txt', (err) => {
   if (err) throw err;
   console.log('./public/robot_branch.txt was copied to dist/robots.txt');
+});
+unlink('./public/robot_branch.txt', (err) => {
+  if (err) throw err;
+  console.log('./public/robot_branch.txt was deleted');
+});
+unlink('./public/robot_prod.txt', (err) => {
+  if (err) throw err;
+  console.log('./public/robot_prod.txt was deleted');
 });
