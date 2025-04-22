8a315842c86c8e63ad39c14bb0dcd9a281aff662
CarstenADV <carsten.hof@alpinedata.de>
2025-04-22 19:59:01 +0200

    Remove Data Assessment from SiteIndex

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,8 @@ export default defineConfig({
         !page.includes('/categories/') &&
         !page.includes('/page/') &&
         page !== 'https://alpinedata.de/authors/' &&
+        page !== 'https://alpinedata.de/data_assessment/' &&
+        page !== 'https://alpinedata.de/data-assessment/assess/' &&
         page !== 'https://alpinedata.de/contact/'
     }),
     AutoImport({
