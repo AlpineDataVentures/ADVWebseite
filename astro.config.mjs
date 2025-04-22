40875189cb15f12da5f04fa4ae5e8c614b35bebd
CarstenADV <carsten.hof@alpinedata.de>
2025-04-22 08:19:44 +0200

    Remove unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,6 @@ export default defineConfig({
         "@/shortcodes/Accordion",
         "@/shortcodes/Notice",
         "@/shortcodes/Video",
-        "@/shortcodes/Youtube",
         "@/shortcodes/Tabs",
         "@/shortcodes/Tab",
       ],
