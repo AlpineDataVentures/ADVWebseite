5360220f7283d71a41fe1666f049f730ddf6a511
CarstenADV <carsten.hof@alpinedata.de>
2025-04-23 23:48:39 +0200

    Try with npm sharp install ...

--- netlify-build-setup.sh
+++ netlify-build-setup.sh
@@ -3,7 +3,7 @@
 if [ "$CI" = "true" ]; then
   echo "Running in CI – setting up for Netlify"
   rm -rf node_modules yarn.lock
-  yarn add sharp
+  npm install sharp --platform=linux --arch=x64 --ignore-scripts=false
 else
   echo "Skipping Netlify setup – not in CI"
 fi
