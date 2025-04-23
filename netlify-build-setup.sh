ac78a6d8ddc571b28e3e7f40e11cf52482df789c
CarstenADV <carsten.hof@alpinedata.de>
2025-04-23 23:13:49 +0200

    Try changes in pre-build

--- netlify-build-setup.sh
+++ netlify-build-setup.sh
@@ -3,8 +3,7 @@
 if [ "$CI" = "true" ]; then
   echo "Running in CI – setting up for Netlify"
   rm -rf node_modules yarn.lock
-  yarn install --ignore-scripts
-  yarn add sharp --platform=linux --arch=x64
+  yarn add sharp
 else
   echo "Skipping Netlify setup – not in CI"
 fi
