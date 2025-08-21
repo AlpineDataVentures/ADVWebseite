8731f0319ec90fa97c97bd53a0226d9c92b26c04
CarstenADV <carsten.hof@alpinedata.de>
2025-08-21 22:42:22 +0200

    Migrate to latest collection mechanism in Astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,42 +3,36 @@ import { defineCollection, z } from "astro:content";
 
 // schema of the about section
 const aboutCollection = defineCollection({
-  schema: z.discriminatedUnion('type', [
-    z.object({
-      type: z.literal('index'),
-      title: z.string(),
-      meta_title: z.string(),
-      description: z.string(),
-      image: z.string().optional(),
-      draft: z.boolean().optional(),
-    }),
-    z.object({
-      type: z.literal('collaboration'),
+  loader: glob({ pattern: "**/-*.{md,mdx}", base: "src/content/about" }),
+  schema: z.object({
+    title: z.string(),
+    meta_title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    draft: z.boolean().optional(),
+    collaboration: z.object({
       title: z.string(),
       content: z.string(),
     }),
-    z.object({
-      type: z.literal('corevalues'),
-      corevalues: z.array(
-        z.object({
-          title: z.string(),
-          icon: z.string(),
-          description: z.string(),
-        })
-      ),
-    }),
-    z.object({
-      type: z.literal('vision_method'),
+    corevalues: z.array(
+      z.object({
+        title: z.string(),
+        icon: z.string(),
+        description: z.string(),
+      })
+    ),
+    vision_method: z.object({
       title_1: z.string(),
       content_1: z.string(),
       title_2: z.string(),
-      content_2: z.string(),
-    }),
-  ]),
+      content_2: z.string()
+    })
+  }),
 });
 
 // Author collection schema
 const authorsCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/authors" }),
   schema: z.object({
     title: z.string(),
     blog_name: z.string(),
@@ -86,9 +80,11 @@ const blogCollection = defineCollection({
 
 // Collection schema for ADV CaseStudies
 const casestudiesCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/casestudies" }),
   schema: z.object({
     title: z.string(),
     meta_title: z.string().optional(),
+    description: z.string().optional(),
     image: z.string().optional(),
     draft: z.boolean().optional(),
     summary: z.string().optional(),
@@ -97,6 +93,7 @@ const casestudiesCollection = defineCollection({
 
 // contact collection
 export const contactCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/contact" }),
   schema: z.object({
     title: z.string(),
     meta_title: z.string(),
@@ -108,6 +105,7 @@ export const contactCollection = defineCollection({
 
 
 export const datareadyCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/dataready" }),
   schema: z.object({
     title: z.string(),
     meta_title: z.string(),
@@ -139,6 +137,7 @@ export const datareadyCollection = defineCollection({
 });
 
 export const dictionaryCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/dictionary" }),
   schema: z.object({
     items: z.array(
       z.object({
@@ -151,6 +150,7 @@ export const dictionaryCollection = defineCollection({
 });
 
 export const homepageCollection = defineCollection({
+  loader: glob({ pattern: "**/-*.{md,mdx}", base: "src/content/homepage" }),
   schema: z.object({
     banner: z.object({
       title1: z.string(),
@@ -181,6 +181,7 @@ export const homepageCollection = defineCollection({
 
 // Pages collection schema
 const pagesCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/pages" }),
   schema: z.object({
     title: z.string(),
     meta_title: z.string().optional(),
@@ -192,6 +193,7 @@ const pagesCollection = defineCollection({
 
 // Collection of ADV Portfolio
 const portfolioCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/portfolio" }),
   schema: z.object({
     title: z.string(),
     title_image: z.string().optional(),
@@ -227,75 +229,87 @@ const portfolioCollection = defineCollection({
   }),
 });
 
-export const sectionsCollection = defineCollection({
-  schema: z.union([
-    // Schema für Call-to-Action
-    z.object({
-      type: z.literal("call-to-action"),
+// Call to Action collection schema
+const ctaSectionCollection = defineCollection({
+  loader: glob({
+    pattern: "call-to-action.{md,mdx}",
+    base: "src/content/sections",
+  }),
+  schema: z.object({
+    enable: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    button: z.object({
       enable: z.boolean(),
-      title: z.string(),
-      image: z.string(),
-      description: z.string(),
-      button: z.object({
-        enable: z.boolean(),
-        label: z.string(),
+      label: z.string(),
+      link: z.string(),
+    }),
+  }),
+});
+
+// Call to Action collection schema
+const cardSectionCollection = defineCollection({
+  loader: glob({
+    pattern: "service_cards.{md,mdx}",
+    base: "src/content/sections",
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    services: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string(),
         link: z.string(),
-      }),
-    }),
-    // Schema für Service Cards
-    z.object({
-      type: z.literal("service_cards"),
-      title: z.string(),
-      description: z.string(),
-      services: z.array(
-        z.object({
-          title: z.string(),
-          description: z.string(),
-          image: z.string(),
-          link: z.string(),
-        })
-      ),
-    }),
-    // Schema für Testimonials
-    z.object({
-      type: z.literal("testimonial"),
-      enable: z.boolean(),
-      title: z.string(),
-      description: z.string(),
-      testimonials: z.array(
-        z.object({
-          name: z.string(),
-          designation: z.string(),
-          avatar: z.string(),
-          content: z.string(),
-        })
-      ),
-    }),
-  ]),
+      })
+    ),
+  }),
 });
 
 const teamCollection = defineCollection({
-  schema: z.discriminatedUnion('type', [
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/team" }),
+  schema:
     z.object({
       type: z.literal('index'),
       title: z.string(),
       meta_title: z.string(),
       description: z.string(),
-      image: z.string().optional(),
-    }),
-    z.object({
-      type: z.literal('call-to-action'),
-      enable: z.boolean(),
-      title: z.string(),
       image: z.string(),
-      description: z.string(),
-      button: z.object({
+      callToAction: z.object({
         enable: z.boolean(),
-        label: z.string(),
-        link: z.string(),
-      }),
+        title: z.string(),
+        image: z.string(),
+        description: z.string(),
+        button: z.object({
+          enable: z.boolean(),
+          label: z.string(),
+          link: z.string(),
+        }),
+      })
     }),
-  ]),
+});
+
+// Testimonials Section collection schema
+const testimonialSectionCollection = defineCollection({
+  loader: glob({
+    pattern: "testimonial.{md,mdx}",
+    base: "src/content/sections",
+  }),
+  schema: z.object({
+    enable: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    testimonials: z.array(
+      z.object({
+        name: z.string(),
+        avatar: z.string(),
+        designation: z.string(),
+        content: z.string(),
+      }),
+    ),
+  }),
 });
 
 // Export collections
@@ -310,6 +324,10 @@ export const collections = {
   homepage: homepageCollection,
   pages: pagesCollection,
   portfolio: portfolioCollection,
-  sections: sectionsCollection,
   team: teamCollection,
+
+  // sections
+  cardSection: cardSectionCollection,
+  ctaSection: ctaSectionCollection,
+  testimonialSection: testimonialSectionCollection,
 };
